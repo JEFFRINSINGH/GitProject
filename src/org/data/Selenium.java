d1bad0a24e92ca39adab9776f43397dba1ddb085
JEFFRINSINGH <jeffrinsingh05@gmail.com>
2022-04-29 21:36:22 +0530

    Second update

--- src/org/data/Selenium.java
+++ src/org/data/Selenium.java
@@ -5,6 +5,8 @@ public class Selenium {
 	public static void main(String[] args) {
 		System.out.println("Java");
 		System.out.println("Pythan");
+		System.out.println("sql");
+		System.out.println("api");
 	}
 
 }
